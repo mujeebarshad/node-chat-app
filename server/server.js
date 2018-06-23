96e94897ff8ff798a66c1b33c5f4348da059f0d6
mujeebarshad <mujeebarshad@hotmail.com>
2018-06-23 22:02:21 +0500

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,11 +14,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'mike@example.com',
+        text: 'Hey, What is going on.',
+        createdAt: 123 
+    });
+
+   socket.on('createMessage', (message) => {
+       console.log('createMessage', message);
+   });
+
     socket.on('disconnect', () => {
-        console.log('Disconnected from server');
+        console.log('User was disconnected');
     });
 });
-
 
 
 server.listen(port, () => {

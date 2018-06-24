cd1079da04ec8159b9b07fdcf9834e7805d98bcb
mujeebarshad <mujeebarshad@hotmail.com>
2018-06-24 23:00:49 +0500

    Location added

--- public/js/index.js
+++ public/js/index.js
@@ -18,6 +18,15 @@ socket.on('newMessage', function(message) {
 });
 
 
+socket.on('newLocationMessage', function (message) {
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My current location</a>');
+
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('#message').append(li);
+});
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
@@ -29,3 +38,20 @@ jQuery('#message-form').on('submit', function (e) {
 
     });
 });
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+    if (!navigator.geolocation) {
+        return alert('Geoloaction not supported by your browser.');
+    }
+    
+    navigator.geolocation.getCurrentPosition(function(position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function () {
+        alert('Unable to fetch location');
+    });
+});
+
